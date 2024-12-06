f4d0abf425a23759ed10813288ac4e7e152845d0
Forno Alan <fornoalan@gmail.com>
2024-12-06 14:24:22 -0300

    biometrico funcionando

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -9,6 +9,8 @@ import {
   BiometryErrorType,
   BiometricAuth,
 } from '@aparajita/capacitor-biometric-auth';
+import { PluginListenerHandle } from '@capacitor/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -18,33 +20,52 @@ import {
   imports: [IonGrid, IonRow, IonCol, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonInput, IonButton]
 })
 export class LoginPage implements OnInit {
+  appListener!: PluginListenerHandle;
+  public mensajeError: any;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.validarSoporteBiometrico();
   }
 
   usarHuella() {
-
+    this.autenticar().then(() => {
+      this.router.navigate(['/tab1/tab1']);
+    }).catch((error) => {
+      this.mensajeError = "Ocurrio un error al autenticar";
+    });
   }
 
-  async authenticate(): Promise<void> {
+  isInfoBiometricaDisponible(info: CheckBiometryResult): boolean {
+    return info.isAvailable;
+  }
+
+  async validarSoporteBiometrico() {
+    this.isInfoBiometricaDisponible(await BiometricAuth.checkBiometry());
+
+    try {
+      this.appListener = await BiometricAuth.addResumeListener(this.isInfoBiometricaDisponible);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async autenticar(): Promise<void> {
     try {
       await BiometricAuth.authenticate({
-        reason: 'Please authenticate',
-        cancelTitle: 'Cancel',
+        reason: 'Autenticarse',
+        cancelTitle: 'Cancelar',
         allowDeviceCredential: true,
         iosFallbackTitle: 'Use passcode',
-        androidTitle: 'Biometric login',
-        androidSubtitle: 'Log in using biometric authentication',
+        androidTitle: 'Login biometrico',
+        androidSubtitle: 'Ingresa usando autenticacion biometrica',
         androidConfirmationRequired: false,
         androidBiometryStrength: AndroidBiometryStrength.weak,
       })
     } catch (error) {
       if (error instanceof BiometryError) {
-        if (error.code !== BiometryErrorType.userCancel) {
-          
-        }
+        throw error;
       }
     }
   }
